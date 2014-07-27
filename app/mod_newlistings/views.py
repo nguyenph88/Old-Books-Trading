63523435e9057161490cb5032a60413c381d4b1b
Peter Nguyen <nguyenph88@gmail.com>
2014-07-27 00:19:24 -0700

    added template for viewlisting

--- app/mod_newlistings/views.py
+++ app/mod_newlistings/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #############################################################
 # 
 # Author: Peter Nguyen
@@ -11,9 +12,24 @@
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 from werkzeug import check_password_hash, generate_password_hash
 
-##### !!!!! Important !!!!!
+from app import db, lm
+
+###################################
+## Initial setup for this module ##
+###################################
+
+# Register Blue print
 mod = Blueprint('newlistings', __name__, url_prefix='/newlistings')
 
+# Set the page that @login_required will redirect to
+lm.login_view = 'users.dangnhap'
+
+# Display this message when user not login and try to connect to #login_required
+lm.login_message = u"Xin vui lòng đăng nhập để tiếp tục."
+
+###################################
+########## View Handling ##########
+###################################
 @mod.route('/')
 def newlistings():
   return render_template("newlistings/newlistings.html")
