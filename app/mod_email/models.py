71552bb98632ecddb07e9bef8b4c8d2100dd0b2b
Peter Nguyen <nguyenph88@gmail.com>
2014-08-03 13:11:39 -0700

    added new folder mod_email

---
+++ app/mod_email/models.py
@@ -0,0 +1,8 @@
+#############################################################
+# 
+# Author: Peter Nguyen
+# Last Update: 08/02/2014
+# Description:   - Model for this module
+#                - Initialize with sqlite and basic column
+# 
+#############################################################
---
+++ app/mod_email/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#############################################################
+# 
+# Author: Peter Nguyen
+# Last Update: 07/20/2014
+# Description:   - Define how each route is handled
+#                - Handle all http/get/post requests
+# 
+#############################################################
